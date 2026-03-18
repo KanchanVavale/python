b63d7cd07257461fd88d36ce1dbf80512127ab88
Nishant Gite <kanchanvavale20@gmail.com>
2026-03-18 16:10:55 +0900

    updated commit

--- Kanchan_assignment3/Task2_math_operation.py
+++ Kanchan_assignment3/Task2_math_operation.py
@@ -1,16 +1,24 @@
 #calculation using math module.
 import math
 
-num = int(input("Enter any number: "))
+try:
+    num = int(input("Enter any number: "))
+    if num <= 0:
+        print("Please enter positive number")
+    else:
+        #Square root of the number
+        squareroot = math.sqrt(num)
+        print("Square root: ", squareroot)
 
-#Square root of the number
-squareroot = math.sqrt(num)
-print("Square root: ", squareroot)
+        #Natural logarithm (log base e) of the number
+        log_of_num = math.log(num)
+        print("Logarithm: ", log_of_num)
 
-#Natural logarithm (log base e) of the number
-log_of_num = math.log(num)
-print("Logarithm: ", log_of_num)
+        #Sine of the number (in radians)
+        sin_of_num = math.sin(num)
+        print("Sine: ", sin_of_num)
 
-#Sine of the number (in radians)
-sin_of_num = math.sin(num)
-print("Sine: ", sin_of_num)
+
+except ValueError:
+    print("Invalid Input! Please enter only positive number")
+
--- Kanchan_assignment3/factorial.py
+++ Kanchan_assignment3/factorial.py
@@ -1,36 +1,25 @@
 #Factorial using recursion 
 def fact(num):
     """
-    Calculate the factorial of a number using recursion.
-
-    Parameters:
-    num (int): The number whose factorial needs to be calculated.
-
-    Returns:
-    int: Factorial of the number if valid
-    None: If the number is negative
+    FUNCTION TO CALCULATE FACTORIAL OF A NUMBER
     """
-
     if num < 0:
-        print("Error: Factorial is not defined for negative numbers.")
+        print("Please enter positive number.")
         return None
     elif num == 0 or num == 1:
         return 1
     else:
         return num * fact(num - 1)
 
+try:
+    num = int(input("Enter a number: "))
+    result = fact(num)
 
-if __name__ == "__main__":
-    
-    try:
-        num = int(input("Enter a number: "))
-        result = fact(num)
+    if result is not None:
+        print(f"Factorial of {num} is: {result}")
 
-        if result is not None:
-            print(f"Factorial of {num} is: {result}")
-
-    except ValueError:
-        print("Error: Please enter a valid integer.")
+except ValueError:
+    print("Please enter a number")
 
 
 
